b5465d8010c0e148de3b9b0f599bc3ba6e21cc0d
秋之穹雪曜 <101522209+YukiyoAutumnaether@users.noreply.github.com>
2022-11-23 16:56:13 +0800

    Add files via upload

---
+++ Test/src/test/java/org/apache/commons/math3/util/FastMathTest.java
@@ -0,0 +1,29 @@
+package org.apache.commons.math3.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FastMathTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testSqrt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAbsInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMinIntInt() {
+		fail("Not yet implemented");
+	}
+
+}
